0c7cada389ef7f55f4bbe67438ef948140c34690
AlexKVal <alexkval@gmail.com>
2014-08-23 16:51:02 +0300

    eigenclass class << self

--- lib/rack/handler.rb
+++ lib/rack/handler.rb
@@ -8,82 +8,84 @@ module Rack
   # A second optional hash can be passed to include server-specific
   # configuration.
   module Handler
-    def self.get(server)
-      return unless server
-      server = server.to_s
+    class << self
+      def get(server)
+        return unless server
+        server = server.to_s
 
-      unless @handlers.include? server
-        load_error = try_require('rack/handler', server)
+        unless @handlers.include? server
+          load_error = try_require('rack/handler', server)
+        end
+
+        if klass = @handlers[server]
+          klass.split("::").inject(Object) { |o, x| o.const_get(x) }
+        else
+          const_get(server)
+        end
+
+      rescue NameError => name_error
+        raise load_error || name_error
       end
 
-      if klass = @handlers[server]
-        klass.split("::").inject(Object) { |o, x| o.const_get(x) }
-      else
-        const_get(server)
+      # Select first available Rack handler given an `Array` of server names.
+      # Raises `LoadError` if no handler was found.
+      #
+      #   > pick ['thin', 'webrick']
+      #   => Rack::Handler::WEBrick
+      def pick(server_names)
+        server_names = Array(server_names)
+        server_names.each do |server_name|
+          begin
+            return get(server_name.to_s)
+          rescue LoadError, NameError
+          end
+        end
+
+        raise LoadError, "Couldn't find handler for: #{server_names.join(', ')}."
       end
 
-    rescue NameError => name_error
-      raise load_error || name_error
-    end
+      def default(options = {})
+        # Guess.
+        if ENV.include?("PHP_FCGI_CHILDREN")
+          # We already speak FastCGI
+          options.delete :File
+          options.delete :Port
 
-    # Select first available Rack handler given an `Array` of server names.
-    # Raises `LoadError` if no handler was found.
-    #
-    #   > pick ['thin', 'webrick']
-    #   => Rack::Handler::WEBrick
-    def self.pick(server_names)
-      server_names = Array(server_names)
-      server_names.each do |server_name|
-        begin
-          return get(server_name.to_s)
-        rescue LoadError, NameError
+          Rack::Handler::FastCGI
+        elsif ENV.include?("REQUEST_METHOD")
+          Rack::Handler::CGI
+        elsif ENV.include?("RACK_HANDLER")
+          get(ENV["RACK_HANDLER"])
+        else
+          pick ['thin', 'puma', 'webrick']
         end
       end
 
-      raise LoadError, "Couldn't find handler for: #{server_names.join(', ')}."
-    end
+      # Transforms server-name constants to their canonical form as filenames,
+      # then tries to require them but silences the LoadError if not found
+      #
+      # Naming convention:
+      #
+      #   Foo # => 'foo'
+      #   FooBar # => 'foo_bar.rb'
+      #   FooBAR # => 'foobar.rb'
+      #   FOObar # => 'foobar.rb'
+      #   FOOBAR # => 'foobar.rb'
+      #   FooBarBaz # => 'foo_bar_baz.rb'
+      def try_require(prefix, const_name)
+        file = const_name.gsub(/^[A-Z]+/) { |pre| pre.downcase }.
+          gsub(/[A-Z]+[^A-Z]/, '_\&').downcase
 
-    def self.default(options = {})
-      # Guess.
-      if ENV.include?("PHP_FCGI_CHILDREN")
-        # We already speak FastCGI
-        options.delete :File
-        options.delete :Port
-
-        Rack::Handler::FastCGI
-      elsif ENV.include?("REQUEST_METHOD")
-        Rack::Handler::CGI
-      elsif ENV.include?("RACK_HANDLER")
-        self.get(ENV["RACK_HANDLER"])
-      else
-        pick ['thin', 'puma', 'webrick']
+        require(::File.join(prefix, file))
+        nil
+      rescue LoadError => error
+        error
       end
-    end
 
-    # Transforms server-name constants to their canonical form as filenames,
-    # then tries to require them but silences the LoadError if not found
-    #
-    # Naming convention:
-    #
-    #   Foo # => 'foo'
-    #   FooBar # => 'foo_bar.rb'
-    #   FooBAR # => 'foobar.rb'
-    #   FOObar # => 'foobar.rb'
-    #   FOOBAR # => 'foobar.rb'
-    #   FooBarBaz # => 'foo_bar_baz.rb'
-    def self.try_require(prefix, const_name)
-      file = const_name.gsub(/^[A-Z]+/) { |pre| pre.downcase }.
-        gsub(/[A-Z]+[^A-Z]/, '_\&').downcase
-
-      require(::File.join(prefix, file))
-      nil
-    rescue LoadError => error
-      error
-    end
-
-    def self.register(server, klass)
-      @handlers ||= {}
-      @handlers[server.to_s] = klass.to_s
+      def register(server, klass)
+        @handlers ||= {}
+        @handlers[server.to_s] = klass.to_s
+      end
     end
 
     autoload :CGI, "rack/handler/cgi"
