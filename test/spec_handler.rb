29de191f71a68cd76a2ec0e90ee9e8099837887e
AlexKVal <alexkval@gmail.com>
2014-08-28 18:02:15 +0300

    Added tests for Rack::Handler.handler_opts for refactoring purpose.

--- test/spec_handler.rb
+++ test/spec_handler.rb
@@ -3,6 +3,32 @@ require 'rack/handler'
 class Rack::Handler::Lobster; end
 class RockLobster; end
 
+module Rack
+  module Handler
+    class WithoutOptions
+      # has no method #valid_options for tests purpose
+    end
+    register :without_options, WithoutOptions
+  end
+end
+
+module Rack
+  module Handler
+    class WithOptions
+      def self.valid_options
+        {
+          "Opt1" => "option1 desc",
+          "Opt2" => "option2 desc",
+          "Opt3" => "option3 desc",
+          "Host=HOST" => "Hostname to listen on (default: localhost)",
+          "Port=PORT" => "Port to listen on (default: 8080)",
+        }
+      end
+    end
+    register :with_options, WithOptions
+  end
+end
+
 describe Rack::Handler do
   it "has registered default handlers" do
     Rack::Handler.get('cgi').should.equal Rack::Handler::CGI
@@ -55,5 +81,40 @@ describe Rack::Handler do
     ensure
       $LOAD_PATH.delete path
     end
+  end
+
+  describe '#handler_opts' do
+    should "return empty string when handler is without options" do
+      Rack::Handler.handler_opts({:server => 'without_options'}).should.equal ''
+    end
+
+    should "return text with handler options when handler is with options" do
+      result = Rack::Handler.handler_opts({:server => 'with_options'})
+      result.should.not.equal ''
+      result.should.match(/Opt1/)
+      result.should.match(/Opt2/)
+      result.should.match(/Opt3/)
+    end
+
+    should "ignore Host and Port options of handler" do
+      result = Rack::Handler.handler_opts({:server => 'with_options'})
+      result.should.not.equal ''
+      result.should.not.match(/Host/)
+      result.should.not.match(/Port/)
+    end
+
+    # it shouldn't be acting this way:
+    it "raises LoadError, for now, but it is wrong; when handler was not found" do
+      lambda {
+        Rack::Handler.new.handler_opts({:server => 'non_exsisting'})
+      }.should.raise LoadError
+    end
+
+    # it should be acting this way:
+    # should "return Warning-text when handler was not found" do
+    #   result = Rack::Server::Options.new.handler_opts({:server => 'non_exsisting'})
+    #   result.should.not.equal ''
+    #   result.should.match(/Warning/)
+    # end
   end
 end
